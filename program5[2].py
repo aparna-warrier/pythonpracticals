14172188fb922df2cc811d2983ee4e80b3dc5847
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2022-10-30 12:57:41 +0530

    Add files via upload

---
+++ program5[2].py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a=5
+b=10
+print("before swapping values of a:")
+print(a)
+print("before swapping values of b:")
+print(b)
+a,b=b,a
+print("after swapping value of a")
+print(a)
+print("after swapping value of b")
+print(b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
