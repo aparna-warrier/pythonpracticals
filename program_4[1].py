ccb9e535c833bd42b80e6435c9721998cbf2b9d7
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2022-10-30 12:57:01 +0530

    Add files via upload

---
+++ program_4[1].py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=input("enter the value for a")
+b=input("enter the value for b")
+temp=a
+a=b
+b=temp
+print("value of x after swap is:")
+print(a)
+print("value of y after swap is:")
+print(b)
+
+
+# In[ ]:
+
+
+
+
