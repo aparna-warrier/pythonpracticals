8b493ca17aedcae968f745a7d89e1d749a2a7ae1
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2022-10-30 12:59:12 +0530

    Add files via upload

---
+++ program7A-1[1].py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=int(input("enter the first side"))
+b=int(input("enter the second side"))
+c=int(input("enter the third side"))
+s=(a+b+c)/2
+n=(s*(s-a)*(s-b)*(s-c))**0.5
+print("area of a triangle is:",n)
+
+
+# In[ ]:
+
+
+
+
