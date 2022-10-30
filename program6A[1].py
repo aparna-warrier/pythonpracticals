d8fabc476bc8243df5cc41869562aaf05d2dabbb
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2022-10-30 12:58:30 +0530

    Add files via upload

---
+++ program6A[1].py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import cmath
+a=int(input("enter the first value"))
+b=int(input("enter the second value"))
+c=int(input("enter the third value"))
+d=(b**2)-(4*a*c)
+sol1=(-b+cmath.sqrt(d))/(2*a)
+sol2=(-b-cmath.sqrt(d))/(2*a)
+print("the solution of quadratic equation are", sol1,sol2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
