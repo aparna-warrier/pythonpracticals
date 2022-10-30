81834dfbbcced20c533707e5e902b31da4f58a9e
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2022-10-30 12:56:13 +0530

    Add files via upload

---
+++ program3[1].py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=input("enter first number")
+b=input("enter second number")
+sum=int(a)+int(b)
+print("sum is:")
+print(sum)
+
+
+# In[ ]:
+
+
+
+
