a9ee248b3f7f104f4399a4579335a86261c378b1
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2022-11-05 10:05:34 +0530

    Add files via upload

---
+++ tutorial2A.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+num=int(input("Enter a no:"))
+if(num>0):
+    print("{} is a positive number".format(num))
+elif(num==0):
+    print("{} is zero".format(num))
+else:
+    print("{} is a negative number".format(num))
+
+
+# In[ ]:
+
+
+
+
