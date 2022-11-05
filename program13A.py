db4d3175e559ee223a511e93692fbe67c9e1e63d
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2022-11-05 10:04:32 +0530

    Add files via upload

---
+++ program13A.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+num=int(input("enter the number"))
+flag=False
+for i in range(2,num):
+    if(num%i==0):
+        flag=True
+        break
+if flag:
+        print(num,"is not a prime number")
+else:
+        print(num,"is a prime number")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
