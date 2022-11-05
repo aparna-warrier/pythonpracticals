a6d302b2e02efad37f30dfb1db1924f80f367ef4
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2022-11-05 10:05:11 +0530

    Add files via upload

---
+++ tutorial1A.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+num=int(input("Enter a no:"))
+if(num%2==0):
+    print("{} is a even number".format(num))
+else:
+    print("{} is a odd number".format(num))
+    
+
+
+# In[ ]:
+
+
+
+
