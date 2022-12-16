2c2fb62223d2d6b94cafbba72d93d0440df8af7d
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2022-12-16 10:33:04 +0530

    Add files via upload

---
+++ program37.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x={"name":"LEKSHMI","age":21,"year":2001}
+y={"sub":"MCA","year":2022,"class":3}
+print("first dictionary",x)
+print("second dictionary",y)
+x.update(y)
+print("dictionary after merging",x)
+
+
+# In[ ]:
+
+
+
+
