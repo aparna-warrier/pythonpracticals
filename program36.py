2dc2b082dd0bc16f46c9a155dcc482784f15ff78
Aparna-warrier <116961716+aparna-warrier@users.noreply.github.com>
2023-01-06 09:23:01 +0530

    Add files via upload

---
+++ program36.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+a = {'Anu': 20, 'Aparna': 30, 'Dia': 25, 'Sneha': 23}
+print("Dictionary : ",a)
+print("ascending order using keys : ",sorted(a.keys()))
+print("ascending order using values : ",sorted(a.values()))
+print("descending order using keys :",sorted(a.keys(),reverse=True))
+print("descending order using values :",sorted(a.values(),reverse=True))
+print("ascending order using keys and values :",sorted(a.items()))
+print("descending order using keys and values :",sorted(a.items(),reverse=True))
+
+
+# In[ ]:
+
+
+
+
